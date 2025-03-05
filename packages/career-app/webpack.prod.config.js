44a651de66c9ebd422954b32e4f4678e7aa49bd1
dinesh.1410064 <dinesh.1410064@gmail.com>
2025-03-05 10:47:16 +0530

    added career app yml.

--- packages/career-app/webpack.prod.config.js
+++ packages/career-app/webpack.prod.config.js
@@ -1,1 +1,46 @@
-module.exports = require('./webpack.config');
+const commonConfig = require('./webpack.config');
+const { merge } = require('webpack-merge');
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+const ModuleFedrationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, 'tsconfig.json'),
+  []);
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/career/latest/',
+    },
+    resolve: {
+        alias: {
+          ...sharedMappings.getAliases(),
+        }
+    },
+    plugins: [
+        new ModuleFedrationPlugin({
+            name: 'careerApp',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './CareerApp': './src/bootstrap',
+            },
+            shared: share({
+                "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+                "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+                "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+                "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+                "zone.js": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      
+                ...sharedMappings.getDescriptors()
+            })
+        }),
+        sharedMappings.getPlugin()
+    ],
+};
+
+// module.exports = merge(commonConfig, prodConfig);
+module.exports = prodConfig;
